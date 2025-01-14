344f4152bb9c296d202b4a63b138ac7e6296e3cc
= <=>
2025-01-14 10:42:06 +0530

    federated signin

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FederatedSignIn from './FederatedSignIn';
+
+function App() {
+  return (
+    <div className="App">
+      <FederatedSignIn /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FederatedSignIn.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const FederatedSignIn = () => {
+  const handleFederatedSignIn = () => {
+    
+  };
+
+  return (
+    <div>
+      <h1>Hi This is the federated signin</h1>
+      <button onClick={handleFederatedSignIn}>Federated SignIn</button>
+    </div>
+  );
+};
+
+export default FederatedSignIn;
